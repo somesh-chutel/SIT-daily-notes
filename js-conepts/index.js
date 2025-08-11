9ee5c642cbefbc2c9ee0b93d1f865b8efee61e90
srocknik <somesh.chutel90@gmail.com>
2025-08-11 13:26:21 +0530

    el update

--- js-conepts/index.js
+++ js-conepts/index.js
@@ -1,41 +1,17 @@
-
-let num1El = document.getElementById("num1");
-let num2El = document.getElementById("num2");
-let operatorEl = document.getElementById("operator");
-let resultEl = document.getElementById("result");
+let rootEl = document.getElementById("root");
 
 
-function calculateResult(){
 
-    let val1 = parseInt( num1El.value );
-    let val2 = parseInt( num2El.value );
-    let operator = operatorEl.value;
 
-    switch ( operator ){
 
-        case "+" :
-            resultEl.textContent = `Your result is : ${val1 + val2}`;
-            break;
-        case "-" :
-            resultEl.textContent = `Your result is : ${val1 - val2}`;
-            break;
-        case "*" :
-            resultEl.textContent = `Your result is : ${val1 * val2}`;
-            break;
-        case "/" :
-            resultEl.textContent = `Your result is : ${val1 / val2}`;
-            break;
-        default :
-            resultEl.textContent = "Please provide valid operator";
+function onAddEl(){
+    let headingEl = document.createElement("button");
+    headingEl.textContent = "Remove";
+    headingEl.classList.add("btn","btn-danger");
+    headingEl.id = "myBtn";
+    headingEl.onclick = function(){
+        rootEl.removeChild(headingEl);
     }
-
-    num1El.value = "";
-    num2El.value = "";
-    operatorEl.value = "";
-
+    rootEl.appendChild(headingEl);
+    console.log( headingEl );
 }
-
-
-let randomNumber = Math.ceil( Math.random()*10 );
-
-console.log( randomNumber );
